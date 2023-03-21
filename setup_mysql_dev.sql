87cb9e97a3078f53c790d8a9bf84dcc7aa589962
Rehema Owino <sayeakinyi@gmail.com>
2023-03-21 00:57:28 -0700

    MySQL setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- prepares MySQL server for the project
+-- creates a database, new user, user's password, grants the new user privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
