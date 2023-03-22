d23b1cca38350954970ebbb791afbcd35c514816
Rehema Owino <sayeakinyi@gmail.com>
2023-03-22 07:50:44 -0700

    DBStorage: update State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    State class
+    Attributes:
+        name: name of the state
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
