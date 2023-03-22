c1286166cdbfb142a2c1b32c2a314deebc63d9b4
Rehema Owino <sayeakinyi@gmail.com>
2023-03-22 08:38:20 -0700

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
+
 """This module defines a class User"""
-from sqlalchemy import String, Column
-from models.base_model import Base, BaseModel
+
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user")
-    reviews = relationship("Review", backref="user")
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
