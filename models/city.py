9319335c2b4364cd140584601c44316fe2a6076a
Rehema Owino <sayeakinyi@gmail.com>
2023-03-22 07:50:17 -0700

    DBStorage: update City

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    The city class
+    Attributes:
+        state_id: unique state identifier
+        name: name of the state
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state = relationship("State", backref="cities",
+                         cascade="all, delete-orphan")
